ecf8be207ff297cec5257610e3b58e71e417b39a
Fernweh99 <daniele.dimento@gmail.com>
2022-07-11 15:23:16 +0200

    finish bonus

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 /*
 Descrizione:
 Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
 
 console.log("Vue OK",Vue)
@@ -9,5 +11,6 @@ const app = new Vue({
   el: "#root",
   data: {
     message: "Vue OK",
+    source: "https://picsum.photos/500/300?random=7"
   }
 })
