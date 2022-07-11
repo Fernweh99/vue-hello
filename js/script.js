42d4f2613d65b71f2f0c17c80bc879a0b918363a
Fernweh99 <daniele.dimento@gmail.com>
2022-07-11 15:19:45 +0200

    finish ex

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+*/
+
+console.log("Vue OK",Vue)
+
+const app = new Vue({
+  el: "#root",
+  data: {
+    message: "Vue OK",
+  }
+})
